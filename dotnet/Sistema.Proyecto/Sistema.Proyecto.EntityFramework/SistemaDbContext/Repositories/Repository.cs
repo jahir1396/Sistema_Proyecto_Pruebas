040b3dc5bb2821adbefb6703a70eb6505794066b
Jahir Lopez <jahir.lopez@ies-systems.com>
2022-11-22 15:34:12 -0600

    Proyecto : se agregan metodos para Repository

--- dotnet/Sistema.Proyecto/Sistema.Proyecto.EntityFramework/SistemaDbContext/Repositories/Repository.cs
+++ dotnet/Sistema.Proyecto/Sistema.Proyecto.EntityFramework/SistemaDbContext/Repositories/Repository.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -25,5 +27,93 @@ namespace Sistema.Proyecto
         {
             return Task.FromResult(GetAllList());
         }
+
+        public virtual List<T> GetAllList(Expression<Func<T,bool>> predicate)
+        {
+            return _unitOfWork.Context.Set<T>().Where(predicate).ToList();
+        }
+
+        public virtual Task<List<T>> GetAllListAsync(Expression<Func<T,bool>> predicate)
+        {
+            return Task.FromResult(GetAllList(predicate));
+        }
+
+        public virtual T FirstOrDefault(Expression<Func<T, bool>> predicate)
+        {
+            return _unitOfWork.Context.Set<T>().FirstOrDefault(predicate);
+        }
+
+        public virtual T Get(params object?[]? id)
+        {
+            var entity = _unitOfWork.Context.Set<T>().Find(id);
+            if (entity == null)
+            {
+                throw new Exception($"No existe una instandica {entity} con id = {id}");
+            }
+
+            return entity;
+        }
+
+        public virtual Task<T> GetAsync(params object?[]? id)
+        {
+            return Task.FromResult(Get(id));
+        }
+
+        public virtual T Load(object id)
+        {
+            return Get(id);
+        }
+
+        public virtual void Insert(T entity)
+        {
+            _unitOfWork.Context.Set<T>().Add(entity);            
+        }
+
+        public virtual async Task InsertAsync(T entity)
+        {
+            await _unitOfWork.Context.Set<T>().AddAsync(entity);
+        }
+
+        public virtual void Update(T entity)
+        {
+            _unitOfWork.Context.Set<T>().Update(entity);
+        }
+
+        public virtual void Delete(T entity)
+        {
+            _unitOfWork.Context.Set<T>().Remove(entity);
+        }
+
+        public virtual void Delete(Expression<Func<T,bool>> predicate)
+        {
+            foreach (var entity in GetAllList(predicate))
+            {
+                Delete(entity);
+            }
+        }
+
+        public virtual int Count()
+        {
+            return GetAllList().Count();
+        }        
+
+        //public virtual async Task DeleteAsync(T entity)
+        //{
+        //    Delete(entity);
+        //    return Task.CompletedTask;
+        //}
+
+        //public virtual async Task UpdateAsync(T entity)
+        //{
+        //    await _unitOfWork.Context.Set<T>().Update(entity);
+        //}
+
+        //public virtual object InsertAndGetId(T entity)
+        //{
+        //    var mEntity = _unitOfWork.Context.Set<T>().Add(entity);
+        //    return mEntity.;
+        //}
+
+
     }
 }
