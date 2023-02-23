2905316acecc614d113d11f880064b12787da69f
Jahir Lopez <jahir.lopez@ies-systems.com>
2023-02-23 15:06:47 -0600

    Proyecto: configuracion de archivos yml para deploy y services en kubernetes

---
+++ dotnet/Sistema.Proyecto/Sistema.Proyecto.Web/Controllers/InicioController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Sistema.Proyecto.Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class InicioController : ControllerBase
+    {
+
+        [HttpGet]
+        public string Get()
+        {
+            return "Welcome to the Docker & Kubernetes World!";
+        }
+    }
+}
